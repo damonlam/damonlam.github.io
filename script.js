cc9dbab418fc02dc324eddd706ee854954f6c384
damonlam <dayday95@gmail.com>
2020-08-29 22:30:32 +0800

    Obscuriam Website
    My first commit!

---
+++ script.js
@@ -0,0 +1,85 @@
+"use strict"
+
+function navBar(){
+let page0Title = "home";
+let page1Title = "about";
+let page2Title = "stream";
+let page3Title = "develop";
+let page4Title = "contact";
+
+
+let list0 = document.createElement('li');
+let list1 = document.createElement('li');
+let list2 = document.createElement('li');
+let list3 = document.createElement('li');
+let list4 = document.createElement('li');
+ 
+list0.innerHTML = "<a href='index.html'>" + page0Title +"</a>";
+list1.innerHTML = "<a href='page1.html'>" + page1Title +"</a>";
+list2.innerHTML = "<a href='page2.html'>" + page2Title +"</a>";
+list3.innerHTML = "<a href='page3.html'>" + page3Title +"</a>";
+list4.innerHTML = "<a href='page4.html'>" + page4Title +"</a>";
+
+
+document.getElementById('nav').appendChild(list0);
+document.getElementById('nav').appendChild(list1);
+document.getElementById('nav').appendChild(list2);
+document.getElementById('nav').appendChild(list3);
+document.getElementById('nav').appendChild(list4);
+
+}
+
+function expand(imageSelect){
+    console.log(imageSelect);
+    let chosenImage = imageSelect.id;
+    let imageSource = document.getElementById(chosenImage).src;
+    console.log(imageSource);
+
+    let showDiv = document.getElementById('image-magnifier');
+    showDiv.remove;
+    showDiv.innerHTML = "<img id='big' style='justify-content:right' src='" + imageSource +"'>";
+    document.getElementById('image-magnifier').appendChild(showDiv);
+}
+
+let delayInMilliseconds = 1000;
+// setTimeout(function(){
+
+function retract(imageDeselect){
+    console.log(imageDeselect);
+    let showDiv = document.getElementById('image-magnifier');
+    // let description = document.getElementById('photostream-text').innerHTML;
+    showDiv.innerHTML = "<div id='photostream-text' class='header-icon'><br><img class='header-icon-side' src='icn/cone_of_possibility.png' alt=''><br><p>This is an aggregate photostream pulled from my <a id='not-nav' target='_blank' href='https://unsplash.com/dayday95'>Unsplash</a>, <a id='not-nav' target='_blank' href='https://www.instagram.com/dayday95/'>Instagram</a> and film archive.<br><br>Updates will be made sporadically from time to time. <br><br>&#128270; tap or hover over thumbnail to enlarge</p></div>";
+    document.getElementById('image-magnifier').appendChild(showDiv);
+    // let descriptionTag = document.getElementById('photostream-text');
+    //     description = descriptionTag.innerHTML;
+    // console.log('description' + description);
+    
+    // showDiv.innerHTML = 
+    // showDiv.
+}
+// }, delayInMilliseconds);
+
+function unsplashLink(unsplashSelect){
+    let chosenId = unsplashSelect.id;
+    let unsplashURL = document.getElementById(unsplashSelect).src;
+
+
+}
+
+const navSlide = () => {
+    const burger = document.querySelector('.logo');
+    const nav = document.querySelector('.nav-home');
+    // const navLink = document.querySelectorAll('.nav-home li');
+    //toggle nav
+
+    burger.addEventListener('click',() =>{
+        nav.classList.toggle('nav-active');
+    });
+//     //animate links
+//     navLinks.forEach(link, index=>{
+//         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
+//         console.log(index / 7);
+//     });
+}
+
+navSlide();
