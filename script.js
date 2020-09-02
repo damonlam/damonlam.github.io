52f98690d5088bda0e297c81887685462ec02b97
damonlam <dayday95@gmail.com>
2020-09-02 23:04:48 +0800

    nav bar finished

--- script.js
+++ script.js
@@ -74,17 +74,17 @@ const navSlide = () => {
 
     burger.addEventListener('click',() =>{
         nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
     });
-    navLinks.forEach((link, index) =>{
-        link.style.animation = `navLinkFade 0.5 ease forwards ${index/5}s`
-        console.log(index/5);
-    });
+
+}
 //     //animate links
 //     navLinks.forEach(link, index=>{
 //         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
 //         console.log(index / 7);
 //     });
-}
+    
+
 
     navSlide();
 
