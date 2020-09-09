1cc81a6ea29b705fb2f192bab700e6a858bda1b1
damonlam <dayday95@gmail.com>
2020-09-09 23:42:45 +0800

    compressed print images

--- script.js
+++ script.js
@@ -28,6 +28,20 @@ document.getElementById('nav').appendChild(list3);
 document.getElementById('nav').appendChild(list4);
 
 }
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-else');
+    // const navLink = document.querySelectorAll('.nav-else li');
+    //toggle nav
+
+    burger.addEventListener('click',() =>{
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+        console.log("yes");
+    });
+
+}
+navSlide();
 
 let message = document.getElementById('image-magnifier').innerHTML;
 
@@ -36,12 +50,12 @@ function expand(imageSelect){
     let chosenImage = imageSelect.id;
     let imageSource = document.getElementById(chosenImage).src;
     console.log(imageSource);
-    console.log(message);
     let showDiv = document.getElementById('image-magnifier');
+    // let message = document.getElementById('image-magnifier').innerHTML;
     console.log(message);
     showDiv.remove;
     showDiv.innerHTML = "<img id='big' style='justify-content:right' src='" + imageSource +"'>";
-    document.getElementById('image-magnifier').appendChild(showDiv);
+    // document.getElementById('image-magnifier').appendChild(showDiv);
 }
 
 let delayInMilliseconds = 1000;
@@ -52,17 +66,9 @@ function retract(imageDeselect){
     console.log(imageDeselect);
     let showDiv = document.getElementById('image-magnifier');
     showDiv.remove;
-    // let description = document.getElementById('photostream-text').innerHTML;
     showDiv.innerHTML = message;
-    // "<div id='photostream-text' class='header-icon'> <div class='icon-div'> <img class='header-icon' src='icn/cone_of_possibility.png' alt=''></div><p>This is an aggregate photostream pulled from my <a id='not-nav' target='_blank' href='https://unsplash.com/dayday95'>Unsplash</a>, <a id='not-nav' target='_blank' href='https://www.instagram.com/dayday95/'>Instagram</a> and film archive.<br><br>Updates will be made sporadically from time to time. <br><br>Tap or hover over thumbnail to enlarge &#128270;</p></div>";
-    document.getElementById('image-magnifier').appendChild(showDiv);
+        // document.getElementById('image-magnifier').appendChild(showDiv);
     }
-    // let descriptionTag = document.getElementById('photostream-text');
-    //     description = descriptionTag.innerHTML;
-    // console.log('description' + description);
-    
-    // showDiv.innerHTML = 
-    // showDiv.
 }
 let x = window.matchMedia("(min-width: 901px)")
 // }, delayInMilliseconds);
@@ -73,18 +79,3 @@ function unsplashLink(unsplashSelect){
 
 
 }
-
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-else');
-    const navLink = document.querySelectorAll('.nav-else li');
-    //toggle nav
-
-    burger.addEventListener('click',() =>{
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-        
-    });
-
-}
-navSlide();
